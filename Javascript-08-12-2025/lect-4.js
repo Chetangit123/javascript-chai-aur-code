55b3f58000cfd75da49fa5255732f2665bc5b103
Chetangit123 <pchetan839@gmail.com>
2025-12-08 00:28:58 +0530

    first commit

---
+++ Javascript-08-12-2025/lect-4.js
@@ -0,0 +1,10 @@
+//let,var,const
+
+const accountId = 144553
+let accountEmail = "chetan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Indore" // not recommended
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+// Always use const unless you know that value will change or for reassigning value use let
